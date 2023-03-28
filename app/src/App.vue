5a92b9a130a5a08ae8a122a18d0d7484f3848292
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-03-27 23:59:18 -0300

    primeiro commit

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <primeiro />
+</template>
+
+<script>
+import  primeiro from './components/primeiro.vue'
+
+export default {
+  name: 'App',
+  components: {
+    primeiro
+  }
+}
+</script>
---
+++ app/src/components/primeiro.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1> Ola vue!</h1>
+</template>
+
+<script>
+    export default {
+        name: 'Primeiro'
+    }
+</script>
