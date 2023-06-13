4c0fe47a2391cb4e956709965ebd1917443161a0
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-06-13 17:14:20 -0300

    Inicio do projeto

---
+++ app/src/components/Pessoa.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Essa é a descrição da pessoa: {{ name }}</h2>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Pessoa',
+        data() {
+            return {
+                name: 'Vander',
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+</style>
