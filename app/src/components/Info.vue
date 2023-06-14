7758f32e39e1434e506cf99f52808ba12c89f40b
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2023-06-14 17:59:00 -0300

    conceitos de diretivas condicionais v-if, v-for e v-show

--- app/src/components/Info.vue
+++ app/src/components/Info.vue
@@ -1,15 +1,25 @@
 <template>
 <div>
-    <p>Estou trabalhando no momento, utilizando as seguintes tecnologias:</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento! </p>
+    <p v-else>Estou em busca de novas oportunidades</p>
+    <p> Atualmente utilizando as seguintes tecnologias:</p>
     <ul>
         <li>Vue.JS</li>
         <li>JavaScript</li>
         <li>Ionic</li>
     </ul>
+    <p v-show="mostrar_email"> Entre em contato via: {{ email }}</p>
 </div>
 </template>
 <script>
     export default{
-        name: 'Info'
+        name: 'Info',
+        data() {
+            return {
+                esta_trabalhando: true,
+                mostrar_email: true,
+                email:"vandersann@gmail.com"
+            }
+        }
     }
 </script>
